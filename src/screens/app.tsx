c0af4174621152bd038d5def2b00a4afaa35f2f5
shooooooma415 <shoma20040415@icloud.com>
2025-04-08 16:29:00 +0900

    feat: 画像表示させてみた

--- src/screens/app.tsx
+++ src/screens/app.tsx
@@ -1,23 +1,42 @@
-import { registerRootComponent } from 'expo';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { registerRootComponent } from "expo";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  type ImageSourcePropType,
+  Image,
+  View,
+} from "react-native";
+
+const PlaceholderImage =
+  require("@/assets/images/background-image.png") as ImageSourcePropType;
 
 const App = () => {
-    return (
-      <View style={styles.container}>
-        <Text>Open up app.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
       </View>
-    );
-   }
-   
-   const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-   });
-   
-   registerRootComponent(App);
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
+
+registerRootComponent(App);
