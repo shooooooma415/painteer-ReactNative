7c4ffc458946858cce1d891e940a29d6a665f04b
shooooooma415 <shoma20040415@icloud.com>
2025-04-08 18:07:55 +0900

    feat: footerの作成

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { CircleUserRound, SquarePlus, MapPinned } from 'lucide-react-native';
 
 const Footer = () => {
   return (
     <View style={styles.footer}>
-      <Text style={styles.text}>これはFooterです</Text>
+      <TouchableOpacity style={styles.button}>
+        <MapPinned size={30} color="#333" />
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+        <SquarePlus size={30} color="#333" />
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+        <CircleUserRound size={30} color="#333" />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -13,16 +24,30 @@ const styles = StyleSheet.create({
   footer: {
     position: 'absolute',
     bottom: 0,
+    height: 71,
     width: '100%',
-    backgroundColor: '#eee',
-    paddingVertical: 10,
-    alignItems: 'center',
+    backgroundColor: '#cdcaca',
     borderTopWidth: 1,
     borderColor: '#ccc',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    paddingHorizontal: 20,
   },
-  text: {
-    color: '#555',
-    fontSize: 14,
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  addButton: {
+    backgroundColor: '#3b82f6',
+    borderRadius: 9999,
+    padding: 14,
+    marginTop: -30, // 浮かせる
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    shadowOffset: { width: 0, height: 2 },
+    elevation: 5,
   },
 });
 
